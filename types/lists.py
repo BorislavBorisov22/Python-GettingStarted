6dc8c6ac214a04250880bc58d3fb6d910da120aa
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-15 23:29:59 +0200

    added lists and loops examples

---
+++ types/lists.py
@@ -0,0 +1,8 @@
+student_names = ['Mark', 'Jessica']
+print(student_names[0] == 'Mark')
+print(student_names[1] == 'Jessica')
+
+student_names.append('Homer')
+del student_names[2]
+
+print(len(student_names))
---
+++ types/loops.py
@@ -0,0 +1,12 @@
+student_names = ['Ivan', 'Georgi', 'Todor']
+
+for name in student_names:
+    print('Student name is {0}'.format(name))
+
+x = 0
+for index in range(0, 20):
+    x += 10
+
+x = 0
+for index in range(0, 10, 3):
+    print(index)

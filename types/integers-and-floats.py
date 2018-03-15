51174e87b0223733359340b1369c275316c9d87b
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-15 23:06:58 +0200

    added numbers and strings types examples

---
+++ types/integers-and-floats.py
@@ -0,0 +1,8 @@
+intNumber = 3
+floatNumber = 3.4
+
+pi = 3.14
+
+print(int(pi) == intNumber)
+print(intNumber / 2)
+print(float(intNumber) / 2)
---
+++ types/strings.py
@@ -0,0 +1,7 @@
+text = 'Hello python'
+
+print(text.capitalize())
+print(text.replace('python', 'programming with python'))
+print(text.isalpha())
+print('Hello {0}'.format('Python'))
+print('Some, words, seperated, by, commas'.split(', '))

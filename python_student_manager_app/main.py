07fb97406ff39436995849545f4b1c5ad9e8a218
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-17 14:00:53 +0200

    added simplistic flask web app

--- python_student_manager_app/main.py
+++ python_student_manager_app/main.py
@@ -1,4 +1,24 @@
 from hs_student import HighSchoolStudent
+from student import Student
 
-hs_student = HighSchoolStudent('Mark')
-print(hs_student)
+from flask import redirect, render_template, Flask, url_for, request
+
+students = []
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def students_page():
+    if request.method == "POST":
+        new_student_id = request.form.get("student_id", "")
+        new_student_name = request.form.get("student_name", "")
+        new_student = Student(new_student_name, new_student_id)
+        students.append(new_student)
+
+        return redirect(url_for('students_page'))
+    return render_template('index.html', students=students)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
